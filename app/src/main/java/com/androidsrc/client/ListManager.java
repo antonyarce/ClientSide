d5b14a13edaa419346e26548561b120fb4374fe5
risu4 <noticiasnuevayo@gmail.com>
2016-10-07 08:59:17 -0600

    Configuración de la listManager para poder obtener y editar los bytes totales

--- app/src/main/java/com/androidsrc/client/ListManager.java
+++ app/src/main/java/com/androidsrc/client/ListManager.java
@@ -1,9 +1,33 @@
 package com.androidsrc.client;
 
+import java.util.PriorityQueue;
+
 /**
  * Created by allan on 07/10/16.
  */
 
 public class ListManager {
     public static ListaDoble listaBloques = new ListaDoble();
+    private static int BytesTotales;
+    private static int BytesDisponibles;
+
+
+    public static void agregarBloque(int tamano){
+        BytesDisponibles = BytesDisponibles-tamano;
+    }
+
+    public static int getBytesDisponibles() {
+        return BytesDisponibles;
+    }
+    public static void liberarEspacio(int tamano){
+        BytesDisponibles = BytesDisponibles+tamano;
+    }
+
+    public static int getBytesTotales() {
+        return BytesTotales;
+    }
+
+    public static void setBytesTotales(int bytesTotales) {
+        BytesTotales = bytesTotales;
+    }
 }
